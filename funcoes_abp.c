9b025f0d6178e9936cdb3488990be3ffb53d3569
Jackson Hochscheidt <jackson94h@gmail.com>
2015-08-28 15:59:59 -0300

    detalhes sem importancia

--- funcoes_abp.c
+++ funcoes_abp.c
@@ -9,12 +9,22 @@ ARVORE* criaArvore(){
 	return abp;
 }
 
+int valor(){
+	int valor;
+	printf("Informe o valor a ser inserido na árvore:\n");
+	scanf("%d", &valor);
+	return valor;
+}
+
 NODO* buscar(int key, NODO* root){
-
-
-
-
-	return NULL;
+	if(!root) return NULL;
+	else{
+		if(key > root->key)
+			buscar(key, root->dir);
+		else if(key < root->key)
+			buscar(key, root->esq);
+	}
+	return root;
 }
 
 NODO* criaNodo(){
@@ -24,17 +34,14 @@ NODO* criaNodo(){
 	return new;
 }
 
-
-
-NODO* inserir(int key, NODO* root){
+NODO* inserir(int key, NODO* root, ARVORE* tree){
 	
 	NODO *new = criaNodo();
-	
+	new->key = key;
 	if(!new) return NULL;
 	// 1. Se ainda não há raiz. Novo nodo será a raiz
-	if(!root){
-		root = new; 
-		new->key = key;
+	if(!tree->root){
+		tree->root = new; 
 	}else{
 		// 2. Se há raiz. Compara nodo com a raiz:
 		//		a) se nodo for menor que raiz. Chama inserir para sub-árvore a esquerda
@@ -44,7 +51,7 @@ NODO* inserir(int key, NODO* root){
 				root->dir = new;
 			// senão, chama a inserção para a sub-arvore a direita
 			}else{
-				inserir(key, root->dir);
+				inserir(key, root->dir, tree);
 			}
 		}
 		//		b) se nodo for maior que raiz. Chama inserir para sub-árvore a direita
@@ -54,16 +61,40 @@ NODO* inserir(int key, NODO* root){
 				root->esq = new;
 			// senão, chama a inserção para a sub-arvore a direita
 			}else{
-				inserir(key, root->esq);
+				inserir(key, root->esq, tree);
 			}
 		}
 	}
 	return new;
 }
 
-int valor(){
-	int valor;
-	printf("Informe o valor a ser inserido na árvore:\n");
-	scanf("%d", &valor);
-	return valor;
+
+// inacabada
+NODO* remover(int key, NODO* root){
+	NODO *aux;
+	aux = buscar(key, root);
+	// se aux não possui sub-árvores
+	if(!aux->dir && !aux->esq)
+		free(aux);
+	// se aux possui somente uma sub-arvore 
+	else if(aux->dir)
+		
+		
+	// se aux possui sub-árvore a direita e a esquerda
+	
+	return aux;
+
 }
+
+void imprimir(NODO* root){
+	if(root){
+		printf("<");
+		imprimir(root->esq);
+		printf("%d", root->key);
+		imprimir(root->dir);
+		printf(">");
+	}
+}
+
+
+
