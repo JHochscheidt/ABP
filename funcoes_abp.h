ae2b86b7cf9b3909b7127632273a8f352209a49c
Jackson Hochscheidt <jackson94h@gmail.com>
2015-08-29 14:51:31 -0300

    sem detalhes

--- funcoes_abp.h
+++ funcoes_abp.h
@@ -9,10 +9,7 @@
 typedef struct _arvore{
 	struct _nodo *root;
 	int h;
-	
 }ARVORE;
-
-
 
 typedef struct _nodo{
 	int key;
@@ -21,62 +18,43 @@ typedef struct _nodo{
 	struct _nodo *dir;
 }NODO;
 
+// 1. Função para inserção de nodos em uma ABP que armazene números inteiros.
+NODO* inserir(int new, NODO* root, ARVORE* tree);
+
+/* 2. Função que recebe como parâmetro a árvore gerada no exercício 1 
+ * e um valor e procura o valor em um nó da árvore e retorna o fator do nó. */
+int fator(int key, ARVORE* tree);
+ 
+/* 3. Função que recebe como parâmetro a árvore gerada no exercício 1
+ * e retorna uma informação dizendo se a árvore é ou não AVL. */
+int is_AVL(NODO* root);
+
+/* 4. Função que recebe como parâmetro a árvore gerada no exercício 1
+ * e retorna uma informação dizendo se a árvore está ou não completamente balanceada. */
+int is_balanceada(NODO* root);
 
 // Função para criação de árvore
-
 ARVORE* criaArvore(); // retorna um nodo NULL, a raiz da arvore
 
 // Função para criação de nodo
-
-NODO* criaNodo();
+NODO* criaNodo(int key);
 
 // Função que pede o valor a ser inserido na árvore
 int valor();
 
 // Função que percorre a arvore procurando por chave
-
 NODO* buscar(int key, NODO* root);
 
 // Função para remover um nodo da árvore
-
 NODO* remover(int key, NODO* root);
 
 // Função para imprimir a árvore
-
 void imprimir(NODO* root);
 
 // Função que calcula a altura da árvore
-
 int altura(NODO* root);
 
-
-// 1. Função para inserção de nodos em uma ABP que armazene números inteiros.
-
-NODO* inserir(int new, NODO* root, ARVORE* tree);
-
-
-/* 2. Função que recebe como parâmetro a árvore gerada no exercício 1 
- * e um valor e procura o valor em um nó da árvore e retorna o fator
- * do nó.
- */
- 
-int fator(int key, ARVORE* tree);
- 
-/* 3. Função que recebe como parâmetro a árvore gerada no exercício 1
- * e retorna uma informação dizendo se a árvore é ou não AVL.
- */
- 
-int eh_AVL(ARVORE* tree);
-
-/* 4. Função que recebe como parâmetro a árvore gerada no exercício 1
- * e retorna uma informação dizendo se a árvore está ou não
- * completamente balanceada.
- */
- 
-int estah_balanceada(NODO* root);
-
-
-
-
+// Função que calcula o fator de balanceamento de um nó 
+void calcula_fator(NODO* root);
 
 #endif
