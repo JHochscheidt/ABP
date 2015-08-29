08e0b9a7ecd8529830ba220cfd26b1d7daeda726
Jackson Hochscheidt <jackson94h@gmail.com>
2015-08-29 14:51:10 -0300

    criacao de funcao que calcula fator de um nó, toda vez q é inserido um novo nó na arvore

--- funcoes_abp.c
+++ funcoes_abp.c
@@ -17,38 +17,37 @@ int valor(){
 }
 
 NODO* buscar(int key, NODO* root){
+	NODO *aux = root;
 	if(!root) return NULL;
 	else{
-		if(key > root->key)
-			buscar(key, root->dir);
-		else if(key < root->key)
-			buscar(key, root->esq);
+		if(key > aux->key)
+			buscar(key, aux->dir);
+		else if(key < aux->key)
+			buscar(key, aux->esq);
 	}
-	return root;
+	return aux;
 }
 
-NODO* criaNodo(){
+NODO* criaNodo(int key){
 	NODO *new = (NODO*) malloc(sizeof(NODO));
 	new->dir = NULL;
 	new->esq = NULL;
 	new->fat_b = 0;
+	new->key = key;
 	return new;
 }
 
 NODO* inserir(int key, NODO* root, ARVORE* tree){
+	NODO *new = criaNodo(key);; 
 	
-	NODO *new = criaNodo();
-	new->key = key;
-	if(!new) return NULL;
 	// 1. Se ainda não há raiz. Novo nodo será a raiz
 	if(!tree->root){
+		tree->h = 0;
 		tree->root = new;
 	}else{
 		// 2. Se há raiz. Compara nodo com a raiz:
 		//		a) se nodo for menor que raiz. Chama inserir para sub-árvore a esquerda
-		
 		if(key > root->key){
-			root->fat_b++;
 			// se sub-árvore a direita for vazia. Só insere o novo nodo
 			if(!root->dir){ 
 				root->dir = new;
@@ -59,7 +58,6 @@ NODO* inserir(int key, NODO* root, ARVORE* tree){
 		}
 		//		b) se nodo for maior que raiz. Chama inserir para sub-árvore a direita
 		else if(key < root->key){
-			root->fat_b--;
 			// se sub-árvore a esquerda for vazia. Só insere o novo nodo
 			if(!root->esq){ 
 				root->esq = new;
@@ -69,6 +67,7 @@ NODO* inserir(int key, NODO* root, ARVORE* tree){
 			}
 		}
 	}
+	calcula_fator(root);
 	return new;
 }
 
@@ -91,40 +90,71 @@ void imprimir(NODO* root){
 	if(root){
 		printf("<");
 		imprimir(root->esq);
-		printf("%d", root->key);
+		printf("[%d fat %d]", root->key, root->fat_b);
 		imprimir(root->dir);
 		printf(">");
 	}
 }
 
 int fator(int key, ARVORE* tree){
-	
 	NODO *aux = buscar(key, tree->root);
 	if(!aux){
 		printf("Não existe este valor na árvore");
 		return 0;
 	}
-	
-	
 	return aux->fat_b;
 }
 
-int eh_AVL(ARVORE* tree){
+void calcula_fator(NODO* root){
+	if(!root) return;
 	
+	if(!root->esq && !root->dir)
+		root->fat_b = 0;
+	else if(root->esq && !root->dir)
+		root->fat_b = 1 + root->esq->fat_b;
+	else if(!root->esq && root->dir)
+		root->fat_b = -1 - root->dir->fat_b;
+	else root->fat_b = root->esq->fat_b - root->dir->fat_b;	
+}
 
+int is_AVL(NODO* root){
+	
+	if(!root) return 0;// uma arvore vazia é uma arvore balanceada, e por extensão, uma AVL 
+	
+	if(root->fat_b < 2 || root->fat_b > -2) // arvore balanceada
+		return root->fat_b; // 1 se for balanceada, 0 se for totalmente balanceada
+	else if(root->fat_b > 1 || root->fat_b < -1)
+		return root->fat_b; 
+	else{ 
+		if(root->esq)
+			is_AVL(root->esq);
+		if(root->dir)	
+			is_AVL(root->dir);
+	}
+	return 1;	
+}
 
+int is_balanceada(NODO* root){
+	if(!root) return 1; 
+
+	if(root->fat_b)//em uma arvore completamente balanceada o fator de balanceamento de todo e qualquer nodo é 0
+		return 0;
+	else{ 
+		if(root->esq)
+			is_balanceada(root->esq);
+		if(root->dir)	
+			is_balanceada(root->dir);
+	}
 	return 1;
+
 }
 
 int altura(NODO* root){
-	int altura_esq = 0; 
-	int altura_dir = 0;
-	if(root){			
-		altura_dir = 1 + altura(root->dir);
-		altura_esq = altura(root->esq);
+	if((altura(root->dir) - altura(root->esq)) < 2 && (altura(root->dir) - altura(root->esq)) > -2)
+		return (altura(root->dir) - altura(root->esq));
 	
-	}
-	
+	printf("não é balanceada");
+	return (altura(root->dir) - altura(root->esq));	
 }
 
 
